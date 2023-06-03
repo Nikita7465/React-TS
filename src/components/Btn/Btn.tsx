05b5156a4a20ad9678f9a2ad0cfdc25dac427fe7
Nikita <06nekit13@gmail.com>
2023-06-03 18:26:22 +0600

    init

---
+++ src/components/Btn/Btn.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "./btn.module.css";
+
+interface BtnProps {
+  secondClass?: string;
+  btnText?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
+  disabled?: boolean;
+  lbr?: boolean;
+  rbr?: boolean;
+}
+
+const leftBR = {
+  borderTopLeftRadius: "5px",
+  borderTopRightRadius: "0",
+  borderBottomLeftRadius: "5px",
+  borderBottomRightRadius: "0",
+};
+
+const rightBR = {
+  borderTopLeftRadius: "0",
+  borderTopRightRadius: "5px",
+  borderBottomLeftRadius: "0",
+  borderBottomRightRadius: "5px",
+};
+
+const Btn: React.FC<BtnProps> = ({
+  btnText,
+  secondClass,
+  type,
+  onClick,
+  disabled,
+  lbr,
+  rbr,
+}) => {
+  let btnStyles = {};
+
+  if (lbr) {
+    btnStyles = leftBR;
+  } else if (rbr) {
+    btnStyles = rightBR;
+  }
+  return (
+    <button
+      className={`${styles.btn} ${secondClass}`}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      style={btnStyles}
+    >
+      {btnText}
+    </button>
+  );
+};
+
+export default Btn;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./input.module.css";
+
+interface InputProps {
+  secondClass?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  pText?: string;
+  disabled?: boolean;
+  lbr?: boolean;
+  rbr?: boolean;
+}
+
+const leftBR = {
+  borderTopLeftRadius: "5px",
+  borderTopRightRadius: "0",
+  borderBottomLeftRadius: "5px",
+  borderBottomRightRadius: "0",
+};
+
+const rightBR = {
+  borderTopLeftRadius: "0",
+  borderTopRightRadius: "5px",
+  borderBottomLeftRadius: "0",
+  borderBottomRightRadius: "5px",
+};
+
+const Input: React.FC<InputProps> = ({
+  pText,
+  secondClass,
+  type,
+  disabled,
+  lbr,
+  rbr,
+}) => {
+  let inputStyles = {};
+
+  if (lbr) {
+    inputStyles = leftBR;
+  } else if (rbr) {
+    inputStyles = rightBR;
+  }
+
+  return (
+    <input
+      className={`${styles.input} ${secondClass}`}
+      type={type}
+      placeholder={pText}
+      disabled={disabled}
+      style={inputStyles}
+    />
+  );
+};
+
+export default Input;
